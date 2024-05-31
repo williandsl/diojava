0ad52b8f2b0ebc5f7e4d94803fb7def2174a4df4
Willian Lima <willian.slima@mercadolivre.com>
2024-05-31 18:37:22 -0300

    ternario

---
+++ OperadorTernario/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int a,b;
+        a = 5;
+        b =6;
+
+        String resultado = a<=b ? "true" : "false";
+
+        System.out.println(resultado);
+    }
+}
