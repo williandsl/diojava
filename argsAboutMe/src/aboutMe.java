d41db73fd26d125bd1318f8fc29083c153b7f170
Willian Lima <willian.slima@mercadolivre.com>
2024-06-03 17:36:01 -0300

    java basico

---
+++ argsAboutMe/src/aboutMe.java
@@ -0,0 +1,15 @@
+public class aboutMe {
+    public static void main(String[] args) {
+
+        String nome = args [0];
+        String sobreNome = args [1];
+        int idade = Integer.valueOf(args [2]);
+        double altura = Double.valueOf(args [3]);
+
+
+        System.out.println(nome);
+        System.out.println(sobreNome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
