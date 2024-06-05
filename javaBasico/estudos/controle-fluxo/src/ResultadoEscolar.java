f6f0e5e9387504fa31204dc8bf85446d20e1bb89
Willian Lima <willian.slima@mercadolivre.com>
2024-06-05 20:01:02 -0300

    ternario com elseIF

--- javaBasico/estudos/controle-fluxo/src/ResultadoEscolar.java
+++ javaBasico/estudos/controle-fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,20 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 5;
+        int nota = 6;
 
-        if (nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota < 7) {
-            System.out.println("Recuperacao");
-        } else {
-            System.out.println("Reprovado");
-        }
+        // if (nota >= 7) {
+        // System.out.println("Aprovado");
+        // } else if (nota >= 5 && nota < 7) {
+        // System.out.println("Recuperacao");
+        // } else {
+        // System.out.println("Reprovado");
+        // }
+
+        // ternario simples
+        // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        // ternario com elseif
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperacao" : "Reprovado";
+
+        System.out.println(resultado);
     }
 }
