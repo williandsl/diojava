ae205724e5e316802ad72d8003815000cc254160
Willian Lima <willian.slima@mercadolivre.com>
2024-06-10 15:47:12 -0300

    errr

--- javaBasico/estudos/controle-fluxo/src/ExemplosFor.java
+++ javaBasico/estudos/controle-fluxo/src/ExemplosFor.java
@@ -14,12 +14,6 @@ public class ExemplosFor {
         // System.out.println(carneirinhos);
         // }
         // System.out.println("Willian dormiu");
-        int numero = 1;
-
-        for (int x = 1; x < 2; x++) {
-            numero = numero + x;
-        }
-        System.out.println("O valor de número é: " + numero);
 
     }
 }
