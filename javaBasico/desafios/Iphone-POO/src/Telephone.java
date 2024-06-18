8baf6fc865cf60a1441614af3ea8934849d5d29d
Willian Lima <willian.slima@mercadolivre.com>
2024-06-18 20:06:24 -0300

    javaPOO

---
+++ javaBasico/desafios/Iphone-POO/src/Telephone.java
@@ -0,0 +1,13 @@
+class Telephone {
+    public void call(String phoneNumber) {
+        System.out.println("Calling: " + phoneNumber);
+    }
+
+    public void answer() {
+        System.out.println("Answering call");
+    }
+
+    public void startVoicemail() {
+        System.out.println("Starting voicemail");
+    }
+}
